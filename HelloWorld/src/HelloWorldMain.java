bb831faac2a48f9b59feeccf3873323ba8e85757
Rajitha <rajithavardhi2023@gmail.com>
2024-02-07 20:43:53 +0530

    Explanation of hello world

--- HelloWorld/src/HelloWorldMain.java
+++ HelloWorld/src/HelloWorldMain.java
@@ -1,6 +1,7 @@
 public class HelloWorldMain {
 
     public static void main(String[] args) {
+	    //Printing "Hello World!!!" as output in java programming
         System.out.println("Hello World!!!");
     }
 }
