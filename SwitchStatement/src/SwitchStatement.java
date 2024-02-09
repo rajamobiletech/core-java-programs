f8fe56a11ed5e92f2f4651fb462ee9dd2fb9bc14
Raja Duraisamy <rajadura@cisco.com>
2024-02-09 14:58:23 +0530

    switch, break, continue

---
+++ SwitchStatement/src/SwitchStatement.java
@@ -0,0 +1,74 @@
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        switchExample1();
+        switchExample2();   // Error Method
+        switchExample3();
+    }
+
+    public static void switchExample1() {
+        String day = "wednesday";
+        switch (day) {
+            case "sunday":
+                System.out.println("This is holiday");
+                break;
+            case "monday":
+                System.out.println("This is work start day");
+                break;
+            case "wednesday":
+                System.out.println("This is work mid week");
+                break;
+            default:
+                System.out.println("Invaild input");
+                break;
+        }
+    }
+
+    public static void switchExample2() {
+        String day = "monday";
+        switch (day) {
+            case "sunday":
+                System.out.println("This is holiday");
+            case "monday":
+                System.out.println("This is work start day");
+            case "wednesday":
+                System.out.println("This is work mid week");
+            default:
+                System.out.println("Invaild input");
+        }
+    }
+
+    public static void switchExample3() {
+        int a = 5;
+        switch (a) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            default:
+                System.out.println("No matching number");
+                break;
+        }
+    }
+
+    /*
+    // Syntax
+    switch(input) {
+        case value:
+            //statement
+            break;
+        default:
+            //statement
+            break;
+    }
+     */
+
+}
