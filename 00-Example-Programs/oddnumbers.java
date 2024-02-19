271340ea32ae44c33eb0f2ae9c70005c9dac5795
vinaykumar <vinuvinay9505@gmail.com>
2024-02-19 10:50:51 +0530

    Odd Number and String to int Conversion programs

---
+++ 00-Example-Programs/oddnumbers.java
@@ -0,0 +1,13 @@
+public class oddnumbers {
+    public static void main(String[] args) {
+        int n=100;
+        System.out.println("Enter odd numbers from 1 to " +n+ ":" );
+         for(int i=1; i<=n; ++i){
+            if(i%2!=0){
+                System.out.print(i+ " , ");
+            }
+         }
+
+    }
+    
+}
---
+++ 00-Example-Programs/stringtoint.java
@@ -0,0 +1,14 @@
+public class stringtoint {
+    public static void main(String[] args)
+    {
+        String str1 = "783";
+        String str2 = "456";
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        System.out.println( num1 );
+        System.out.println( num2); }
+    
+
+}
