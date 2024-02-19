b04b3cce218b96e0da02a66f8033d658d4a61228
BHARATHBOJI <devangambharath07@gmail.com>
2024-02-19 11:24:56 +0530

    Count digits and swapping Programs

---
+++ 00-Example-Programs/swapnumbers.java
@@ -0,0 +1,16 @@
+public class swapnumbers{
+     public static void main(String[] args) {
+        int a=30,b=10;
+        System.out.println("Before swap");
+        System.out.println(a);
+        System.out.println(b);
+
+        int temp=a;
+            a=b;
+            b=temp;
+          System.out.println("After swap");
+          System.out.println(a);
+          System.out.println(b);  
+
+    }
+}
